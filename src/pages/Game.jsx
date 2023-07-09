a8a241e73277b285236ea043d29d5af86909fbfb
Nándor Czeglédi <you@example.com>
2023-07-09 14:48:27 +0200

    remove test component for error boundary

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import Header from "../components/Header";
 import GameBoard from "../components/GameBoard";
-import ErrorTrigger from "../components/ErrorTrigger";
 
 const Game = () => {
   return (
     <>
       <Header />
       <GameBoard />
-      <ErrorTrigger />
     </>
   );
 };
